56d902487840f684667813268e29959938edcae6
PHANI <siva.palakeerthi@dru.care>
2025-11-13 06:52:01 +0530

    variables

--- 03-env-variables.sh
+++ 03-env-variables.sh
@@ -2,9 +2,9 @@
 export name="phani"
 echo "Name before process kill :: $name"
 
-process_id=pgrep bash
+process_id=$(pgrep bash)
 echo "${process_id}"
-kill process_id
+kill ${process_id}
 
 echo "Name after process kill :: $name"
 
