5e051ef094d0cb7f3b370da7647f5fba1924c1c9
PHANI <siva.palakeerthi@dru.care>
2025-11-13 05:54:54 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,5 +3,12 @@
 project_name="roboshop"
 environment=dev
 
+component1=$1
+component2=$2
+
 echo "Project name is :: ${project_name}"
 echo "environment is :: $environment"
+
+echo "component 1 :: $component1"
+echo "component 2 :: $component2"
+# sh file2.sh catalogue frontend 
