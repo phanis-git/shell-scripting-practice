125e45ceedc8a0820673df61907929c667854e5d
PHANI <siva.palakeerthi@dru.care>
2025-11-13 11:08:21 +0530

    variables

--- 09-colors.sh
+++ 09-colors.sh
@@ -24,17 +24,17 @@ exit 1
 fi
 
 mkdir -p /var/log/shell-script-practice
-log_file="$0.log"
-if [ !$log_file ]; then
-touch  $log_file
+log_file="/var/log/shell-script-practice/$(basename "$0").log"
+if [ ! -f "$log_file" ]; then
+touch  "$log_file"
 fi
 
 dnf list installed mysql 
 if [ $? -ne 0 ]; then
-dnf install mysql -y &>> $log_file
+dnf install mysql -y &>> "$log_file"
 validate $? "Mysql-server"
 else 
-echo "Mysql server is already installed" | tee -a $log_file
+echo "Mysql server is already installed" | tee -a "$log_file"
 fi 
 
 echo "$$"
