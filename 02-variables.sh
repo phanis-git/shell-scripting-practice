a1c253c00da0145eb203f7a3120fb442f17b26cc
PHANI <siva.palakeerthi@dru.care>
2025-11-13 05:58:01 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -12,3 +12,11 @@ echo "environment is :: $environment"
 echo "component 1 :: $component1"
 echo "component 2 :: $component2"
 # sh file2.sh catalogue frontend 
+
+echo "Enter Email Id"
+read email
+echo "Your mail is $email"
+
+echo "Enter Password"
+read -s password
+echo "Your password is $password"
