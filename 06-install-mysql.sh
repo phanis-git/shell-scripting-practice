eb7ee67c61a384c13f77702da83c3905e76bdfdd
PHANI <siva.palakeerthi@dru.care>
2025-11-13 10:31:57 +0530

    variables

--- 06-install-mysql.sh
+++ 06-install-mysql.sh
@@ -16,5 +16,10 @@ echo "Need root privilege"
 exit 1
 fi
 
+dnf list installed mysql
+if [ $? -ne 0 ]; then
 dnf install mysql -y
 validate $? "Mysql-server"
+else 
+echo "Mysql server is already installed"
+fi 
