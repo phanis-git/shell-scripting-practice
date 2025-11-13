f2f8904c799ca56e03a87004ad0238a7c8a54fe6
PHANI <siva.palakeerthi@dru.care>
2025-11-13 07:14:13 +0530

    variables

---
+++ 04-timestamp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+start_time=$(date +%s)
+
+echo "Hi I am calculating difference between start and end time of script"
+end_time=$(date +%s)
+
+total_time=$(${start_time}-${end_time})
+echo "Total time ${total_time}"
+
