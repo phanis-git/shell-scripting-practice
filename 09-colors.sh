3a03be7bfdd6e2db5ced8ec74314867744c3d5b5
PHANI <siva.palakeerthi@dru.care>
2025-11-13 10:54:09 +0530

    variables

--- 09-colors.sh
+++ 09-colors.sh
@@ -2,5 +2,33 @@
 red="\e[31m"
 green="\e[32m"
 normal="\e[0m"
-echo -e "$red RED $normal"
-echo -e "$green Green $normal"
+# echo -e "$red RED $normal"
+# echo -e "$green Green $normal"
+
+# log_file=
+
+validate (){
+    if [ $1 -ne 0 ]; then
+    echo "Error :: installing $2 is $red Failed $normal" 
+    exit 1
+    else 
+    echo "$2 $green Installing successfully $normal"
+    fi
+}
+
+# check user is root are not
+is_root_user=$(id -u)
+if [ $is_root_user -ne 0 ]; then
+echo "Need root privilege"
+exit 1
+fi
+
+dnf list installed mysql &>> 
+if [ $? -ne 0 ]; then
+dnf install mysql -y
+validate $? "Mysql-server"
+else 
+echo "Mysql server is already installed"
+fi 
+
+echo "$$"
