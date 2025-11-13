cba980159f515c05db0769472dfebe6a34a2cb88
PHANI <siva.palakeerthi@dru.care>
2025-11-13 06:55:43 +0530

    variables

--- 03-env-variables.sh
+++ 03-env-variables.sh
@@ -2,9 +2,3 @@
 # export name="phani"      # we should not place in script instead we need to pass in cli
 echo "Name before process kill :: $name"
 
-# process_id=$(pgrep bash)
-# echo "${process_id}"
-# kill ${process_id}
-
-# echo "Name after process kill :: $name"
-
